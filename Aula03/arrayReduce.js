cd4c29aca2847865cdf30a496a451ba0b3b9a6b3
Cristina Jung <cristijung@gmail.com>
2024-07-18 14:46:20 -0300

    arrays 2

--- Aula03/arrayReduce.js
+++ Aula03/arrayReduce.js
@@ -20,6 +20,28 @@ const meninas = ['Ana', 'Bia', 'Lia'];
 const lista = meninos.concat(meninas);
 console.log(lista);
 
+//reduce segunda forma com + 1 parâmetro (3)
+const calcularMedia = (acumulador, valor, index, arr) => {
+    if (index === arr.length - 1) {
+      acumulador = acumulador / arr.length;
+    } else {
+      acumulador += valor;
+    }
+    return acumulador;
+  };
+  
+  const salaJavaScript = [6, 7, 8, 9, 10, 4];
+  const salaJavas = [3, 10, 9, 9, 10, 8];
+  const salaReacts = [5, 10, 8, 3];
+  
+  const mediaSalaJavaScript = salaJavaScript.reduce(calcularMedia, 0, 0);
+  const mediaSalaJavas = salaJavas.reduce(calcularMedia, 0, 0);
+  const mediaSalaReacts = salaReacts.reduce(calcularMedia, 0, 0);
+  
+  console.log("Média da sala de JS:", mediaSalaJavaScript);
+  console.log("Média da sala de Java:", mediaSalaJavas);
+  console.log("Média da sala de React:", mediaSalaReacts);
+
 //set
 //para remover elementos duplicados
 const guris = ['Pafúncio', 'Jorge', 'Marcelo', 'Jorge'];
